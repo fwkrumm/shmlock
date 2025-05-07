c84e73622f9a4d40c3c26a8329327ecb625b0475
N/A <fabiankrumm@web.de>
2025-05-08 00:40:41 +0200

    add specifix linux test

---
+++ tests/test_posix_linux.py
@@ -0,0 +1,44 @@
+"""
+tests of some special cases which occurred on linux
+"""
+import sys
+import time
+import os
+import unittest
+from multiprocessing import shared_memory
+import shmlock
+import shmlock.shmlock_exceptions
+
+class LinuxPosixTests(unittest.TestCase):
+    """
+    test of basics of shmlock package
+
+    Parameters
+    ----------
+    unittest : _type_
+        _description_
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        test init method
+        """
+        self._shm_location = os.path.abspath("/dev/shm")
+        self._shm_name = None
+        super().__init__(*args, **kwargs)
+
+    def setUp(self):
+        """
+        set up the test case
+        """
+        self.assertTrue(os.path.exists(self._shm_location), "shm location does not exist")
+        self._shm_name = str(time.time())
+
+        l = shmlock.ShmLock(self._shm_name)
+        with l:
+            # file should be generated at desired location
+            self.assertTrue(os.path.isfile(os.path.join(self._shm_location,
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
