f971488f94d1e01d7f1bf78b052f68f74b03c587
N/A <fabiankrumm@web.de>
2025-11-15 23:03:57 +0100

    correct version

--- shmlock/__init__.py
+++ shmlock/__init__.py
@@ -33,4 +33,4 @@ from shmlock.shmlock_main import *
 
 # do NOT alter the following line in any way EXCEPT changing
 # the version number. no comments, no rename, whatsoever
-__version__ = "0.0.9"
+__version__ = "4.3.0"
