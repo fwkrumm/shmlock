ec130b946385f64ea081d7a4ec65031cbe467ef2
fwkrumm <ghub_fk@gmx.de>
2026-04-09 01:04:37 +0200

    Add cron scheduled job and update action versions (#32)
    * add new versions and add cron scheduled job
    
    * change time
    
    * update checkout action version and add test dispatcher
    
    * restore
    
    * for testing
    
    * revert
    
    * removing support for EOL versions
    added concurrency check
    
    * version + readme

--- shmlock/__init__.py
+++ shmlock/__init__.py
@@ -33,4 +33,4 @@ from shmlock.shmlock_main import *
 
 # do NOT alter the following line in any way EXCEPT changing
 # the version number. no comments, no rename, whatsoever
-__version__ = "4.4.4"
+__version__ = "5.0.0"
